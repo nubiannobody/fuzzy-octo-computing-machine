881befce1d0e106a79af321ddd42d6f3bbd0b05c
Kween <kweenyoallhiada@gmail.com>
2023-02-03 23:08:34 -0800

    Add completed areBothOdd2.js exercise

---
+++ areBothOdd2.js
@@ -0,0 +1,15 @@
+/*
+Write a function called "areBothOdd".
+Given 2 numbers, "areBothOdd" returns whether or not both of the given numbers are odd.
+var output = areBothOdd(1, 3);
+console.log(output); // --> true
+
+solution using ternary operator
+
+*/
+
+function areBothOdd(num1, num2) {
+
+    return num1 % 2 ===1 && num2 % 2 ===1 ? true : false;
+     
+    }
