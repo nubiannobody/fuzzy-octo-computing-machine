a9136785b6147743f2990cf3dcfd768de60d4fec
Kween <kweenyoallhiada@gmail.com>
2023-02-10 22:04:26 -0800

    Add completed computeAverageLengthOfWords.js

---
+++ computeAverageLengthOfWords.js
@@ -0,0 +1,13 @@
+/*
+Write a function called "computeAverageLengthOfWords".
+Given two words, "computeAverageLengthOfWords" returns the average of their lengths.
+var output = computeAverageLengthOfWords('code', 'programs');
+console.log(output); // --> 6
+*/
+
+function computeAverageLengthOfWords(word1, word2) {
+   let theAverage = (word1.length + word2.length) / 2;
+   return theAverage;
+  }
+  
+  console.log(computeAverageLengthOfWords("shrimp", "sausage"));
