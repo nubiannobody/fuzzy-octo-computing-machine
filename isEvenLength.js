cc02c21b209c6ec5426b10571d6a3f73e84d2a15
Kween <kweenyoallhiada@gmail.com>
2023-02-10 20:04:49 -0800

    Add completed isEvenlength.js exercise

---
+++ isEvenLength.js
@@ -0,0 +1,19 @@
+
+/*
+Write a function called "isEvenLength".
+Given a word, "isEvenLength" returns whether the length of the word is even.
+var output = isEvenLength('wow');
+console.log(output); // --> false
+*/
+
+function isEvenLength(word) {
+    if (word.length % 2 === 0) {
+      return true;
+    } else {
+      return false
+    }
+    
+  }
+
+console.log(isEvenLength("shrimp"));
+console.log(isEvenLength("dog"));
