a4c9c7cf7c19797a9b7749d63051169dd98672cf
Kween <kweenyoallhiada@gmail.com>
2023-02-10 21:56:15 -0800

    Add completed cube.js

---
+++ cube.js
@@ -0,0 +1,14 @@
+
+/*
+Write a function called "cube".
+Given a number, "cube" returns the cube of that number.
+var output = cube(3);
+console.log(output); // --> 27
+*/
+
+function cube(num) {
+let itsCubed = num * num * num;
+return itsCubed;
+  }
+
+console.log(cube(3));
