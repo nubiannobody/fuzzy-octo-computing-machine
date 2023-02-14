396eaf68101bf7014e69cafe663b8ca7e09540a0
Kween <kweenyoallhiada@gmail.com>
2023-02-13 21:42:00 -0800

    Add completed getLastElement.js

---
+++ getLastElement.js
@@ -0,0 +1,21 @@
+/*
+Write a function called "getLastElement".
+Given an array, "getLastElement" returns the last element of the given array.
+Notes:
+* If the given array has a length of 0, it should return 'undefined'.
+var output = getLastElement([1, 2, 3, 4]);
+console.log(output); // --> 4
+*/
+
+function getLastElement(array) {
+  
+    let lastElement = array.length - 1
+    
+    if (array.length === 0) {
+      return undefined;
+    } else {
+      return lastElement;
+    }
+}
+console.log(getLastElement([]));
+console.log(getLastElement([5, 4, 3]));
